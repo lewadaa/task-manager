e5f4d1c3abd13df86d248bad95532d491bec4051
lewadaa <levaev.vadim@mail.ru>
2025-06-16 16:46:45 +0300

    Add Flyway; remove data.sql

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users (
+                       id BIGSERIAL PRIMARY KEY,
+                       username VARCHAR(50) NOT NULL UNIQUE,
+                       password VARCHAR(255) NOT NULL,
+                       role VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE tasks (
+                       id BIGSERIAL PRIMARY KEY,
+                       title VARCHAR(100) NOT NULL,
+                       description TEXT NOT NULL,
+                       status VARCHAR(20) NOT NULL,
+                       created_at TIMESTAMP NOT NULL,
+                       updated_at TIMESTAMP NOT NULL,
+                       user_id BIGINT NOT NULL,
+                       FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+INSERT INTO users (username, password, role) VALUES
+('john', '$2a$12$fvpKUc2aL6k2F9SsdmEs8.LeaQxiiS/XcwOc6ThTxKW9x9si7XOT6', 'ROLE_USER'),
+('alice', '$2a$12$j6ZbzpWnISmYqD8T0V16zeR0kNZbKES.Fw2mS9GC9Q9Q9pWhXOeCa', 'ROLE_ADMIN')
+ON CONFLICT (username) DO NOTHING;
+
+INSERT INTO tasks (title, description, status, created_at, updated_at, user_id)
+VALUES
+  ('Cleaning', 'Clean the room', 'PENDING', NOW(), NOW(), 1),
+  ('Delivery', 'Deliver products to the grocery store', 'IN_PROGRESS', NOW(), NOW(), 2);
